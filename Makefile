0cfd936759acaf5060eb5bc4a82e785cc044a257
Ben Morgan <neembi@gmail.com>
2019-01-15 20:53:50 +0100

    Install a set of useful aliases

--- Makefile
+++ Makefile
@@ -4,3 +4,4 @@ DESTDIR=~/.local/bin
 
 install:
 	install -m755 scanbro.py ${DESTDIR}/scanbro
+	test -d ~/.zdir && install -m644 aliases.sh ~/.zdir/70-scanbro.zsh
---
+++ aliases.sh
@@ -0,0 +1,6 @@
+# Scanning aliases
+alias scan-xs="scanbro -m bw -ccavg low"
+alias scan-s="scanbro -m bw -ccavg medium"
+alias scan-m="scanbro -ccavg medium"
+alias scan-l="scanbro -ccavg high"
+alias scan-xl="scanbro -ccavg extreme"
